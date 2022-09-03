90a8432da74cde96dbeda804893875279b1e6d27
Marius Reusch <marius.reusch1@swisscom.com>
2022-09-03 17:09:07 +0200

    initial commit

---
+++ src/main/java/com/springfundamentals/SpringFundamentalsStartMavenApplication.java
@@ -0,0 +1,13 @@
+package com.springfundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFundamentalsStartMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFundamentalsStartMavenApplication.class, args);
+    }
+
+}
